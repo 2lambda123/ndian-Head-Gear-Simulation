3a6781b6357974214af162c4d0faeec02d3635de
Mitesh-Cpp <58469816+Mitesh-Cpp@users.noreply.github.com>
2022-10-26 17:01:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, Response
-
+import requests
 import camera
 from camera import Video, choice, total_headgears
 
@@ -18,6 +18,33 @@ camera.choice = 0
 def index():
     #global ch
     return render_template('index.html')
+
+@app.route('/culturalNews.html')
+def culturalNews():
+    url = ('https://newsapi.org/v2/everything?'
+           'q=festival&'
+           'from=2022-10-26&'
+           'sortBy=popularity&'
+           'language=en&'
+           'apiKey=8d9dec18335e4d82b8d31756136ebc10')
+    r = requests.get(url).json()
+    case = {
+        'articles' : r['articles']
+    }
+    return render_template("culturalNews.html", cases = case)
+
+@app.route('/start.html')
+def start_the_quiz():
+    return render_template("start.html")
+
+@app.route('/quiz.html')
+def quiz_live():
+    return render_template("quiz.html")
+
+@app.route('/end.html')
+def quiz_end():
+    return render_template("end.html")
+
 
 @app.route('/background_process_test')
 def background_process_test():
@@ -41,4 +68,4 @@ def video():
     return Response(gen(Video()),
     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-app.run(debug=True)
+#app.run(debug=True)
