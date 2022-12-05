4ff9a0413e9ac5dd934a36cccb4f1a728f51a182
Mitesh-Cpp <58469816+Mitesh-Cpp@users.noreply.github.com>
2022-12-05 22:35:35 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -4,8 +4,9 @@ import  cvzone
 cascade =cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 overlays = [cv2.imread('HeadGear_01.png', cv2.IMREAD_UNCHANGED),cv2.imread('HeadGear_02.png', cv2.IMREAD_UNCHANGED),
             cv2.imread('HeadGear_03.png', cv2.IMREAD_UNCHANGED)]
-position_of_headgears = [[10,90],[10,90],[10,90]]
-size_of_headgears = []
+position_of_headgears = [[10,90],[10,90],[20,90]]
+# width, height relative scaling factors
+size_of_headgears = [[1.1,1.1],[1.1,1.1],[1.3,1.1]]
 choice = 0
 total_headgears = len(overlays)
 
@@ -27,7 +28,7 @@ class Video(object):
             for (x, y, w, h) in faces:
                 if (x >= 25 and x <= 460 and y >= 110 and y <= 370):
                     # cv2.rectangle(frame,(x, y), (x+w, y+h), (0, 255, 0), 2)
-                    overlay_resize = cv2.resize(overlays[choice], (int(w * 1.1), int(h * 1.1)))
+                    overlay_resize = cv2.resize(overlays[choice], (int(w * size_of_headgears[choice][0]), int(h * size_of_headgears[choice][1])))
                     frame = cvzone.overlayPNG(frame, overlay_resize, [x - position_of_headgears[choice][0],
                                                                       y - position_of_headgears[choice][1]])
         #if cv2.waitKey(15) == ord('n'):
